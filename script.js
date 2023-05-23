7f6d50b7eeb171c61bf8857aacedef4804960502
pernwn <pernillepjohansen@hotmail.com>
2023-05-23 21:53:07 +0200

    ting og sager er sket med script

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-let image = document.getElementsById('stemning');
-new simpleParallax(image);
+let data;
+const gMorgen = document.getElementById('godmorgen');
+const gEfter = document.getElementById('godEftermiddag');
+const gAften = document.getElementById('godAften');
+
+const array = [gMorgen, gEfter, gAften];
+
+array[0].style.display = "none";
+array[1].style.display = "none";
+array[2].style.display = "none";
+
+fetch('http://worldtimeapi.org/api/timezone/Europe/Copenhagen')
+
+    .then(function (response) {
+        return response.json();
+    })
+
+    //TODO: FIKS DET HERRRRRRRR
+    if(function (obj){
+        data = obj;
+        array[0].innerHTML = "Hello: " + data.timezone;
+    })else if(function (obj){
+        data = obj;
+    })else(function (obj){
+        data = obj;
+    })
+
+
+
+    .catch(function (error) {
+        alert("Nothing");
+    })
+
+
