2f1c5fa74164545cd3b1317f3b9cfaa3710f3674
pernwn <pernillepjohansen@hotmail.com>
2023-05-24 12:53:34 +0200

    javascript:)
    får flip på js jeg kan ik

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const hello = document.getElementById("hello");
 const nfp = document.getElementById("nfp");
+const indivArtist = document.getElementsByClassName("artist");
+const txtContainer = document.getElementsByClassName("artistTxtContainer");
+const artistMere = document.getElementsByClassName("artistMere");
 
 const timeOut = setTimeout(greetMe);
 const stopTime = setTimeout(changeTxt, 4000)
+
 
 
 //VELKOMST AFHÆNGIG AF TID PÅ DAGEN
@@ -11,17 +15,16 @@ function greetMe(){
     let greeting;
     if (time < 10) {
         greeting = "Godmorgen!";
-    } else if (time < 20) {
-        greeting = "God eftermiddag!";
-    } else {
+    } else if (time < 12) {
+        greeting = "God middag!";
+    } else if (time < 18) {
+        greeting = "God eftermiddag!"
+    }else{
         greeting = "God aften!"
     };
 
     nfp.style.display ="none";
     hello.innerHTML = greeting;
-
-
-
     
 }
 
@@ -35,6 +38,38 @@ function changeTxt(){
 
 
 
+//TODO: KAJFLDFJLDAJF VEND TILBGE TIL DET HER
+function hover(){
+    artistMere.style.display = 'block';
+    txtContainer.style.display = 'none';
+}
+
+function standard(){
+    artistMere.style.display = 'none';
+    txtContainer.style.display = 'block';
+}
+
+indivArtist.addEventListener("mouseover", hover);
+indivArtist.addEventListener("mouseout", standard);
+
+
+/*
+indivArtist.onmouseover = function(){
+    mouseOver();
+};
+
+indivArtist.onmouseout = function(){
+    mouseOut();
+};
+
+function mouseOver(){
+    txtContainer.style.visibility = 'hidden';
+}
+
+function mouseOut(){
+    txtContainer.style.visibility = 'visible';
+}
+*/
 
 
 
