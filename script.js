21e0bcea8b3e6d9272734f59da12a85b2b7951f4
pernwn <pernillepjohansen@hotmail.com>
2023-05-29 20:41:14 +0200

    tilføjet seo og rettelse efter validering

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const hello = document.getElementById("hello");
 const nfp = document.getElementById("nfp");
 
-const indivArtist = document.querySelector("artist");
-const txtContainer = document.querySelector("artistTxtContainer");
-const artistMere = document.querySelector("artistMere");
-
 
 //Tryk på pil og scroll ned til om sektion
 function scrollDown() {
@@ -12,24 +8,24 @@ function scrollDown() {
 }
 
 //VELKOMST AFHÆNGIG AF TID PÅ DAGEN – https://www.studentstutorial.com/javascript/javascript-wish-as-per-time.php / https://www.w3schools.com/js/tryit.asp?filename=tryjs_elseif 
-const timeOut = setTimeout(greetMe);
-const timeStop = setTimeout(changeTxt, 3000);
+setTimeout(greetMe);
+setTimeout(changeTxt, 3000);
 
 function greetMe() {
 
     const time = new Date().getHours();
     let greeting;
     if (time < 06) {
-        greeting = "Godnat!"
+        greeting = "Godnat!";
     } else if (time < 10) {
-        greeting = "Godmorgen!"
+        greeting = "Godmorgen!";
     } else if (time < 12) {
-        greeting = "Godmiddag!"
+        greeting = "Godmiddag!";
     } else if (time < 18) {
-        greeting = "Godeftermiddag!"
+        greeting = "Godeftermiddag!";
     } else {
-        greeting = "Godaften!"
-    };
+        greeting = "Godaften!";
+    }
 
     nfp.style.display = "none";
     nfp.style.animationName = "fadeIn";
@@ -43,26 +39,6 @@ function changeTxt() {
     nfp.style.display = "block";
     hello.style.display = "none";
 }
-
-/*Fade in effekt, genbruges når man skriver reveal efter tag
-https://alvarotrigo.com/blog/css-animations-scroll/
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
-        } else {
-            reveals[i].classList.remove("active");
-        }
-    }
-}
-
-window.addEventListener("scroll", reveal);*/
 
 
 
